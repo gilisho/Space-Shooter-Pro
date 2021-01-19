e688b090b8a242593505a62c072a4bf412f03422
gilisho <gilisho@users.noreply.github.com>
2021-01-19 22:10:06 +0200

    add laser behaviour

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // translate laser up
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+}
